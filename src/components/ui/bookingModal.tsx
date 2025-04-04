d4bd50706055d1b2dd94669cf3b95331612b0cb1
itsmiraz <mirajulislamofficial044@gmail.com>
2025-04-04 17:59:20 +0600

    select addult and children added

--- src/components/ui/bookingModal.tsx
+++ src/components/ui/bookingModal.tsx
@@ -8,6 +8,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import RoomImg1 from "@/assets/images/room1.png";
+import { MinusIcon, Plus } from "lucide-react";
 
 import {
   Form,
@@ -24,9 +25,10 @@ const contactFormSchema = z.object({
   name: z.string().min(2, { message: "Name must be at least 2 characters" }),
   email: z.string().email({ message: "Please enter a valid email address" }),
   phone: z.string().min(10, { message: "Please enter a valid phone number" }),
-  message: z
-    .string()
-    .min(10, { message: "Message must be at least 10 characters" }),
+  message: z.string(),
+
+  adults: z.number().min(1, { message: "At least one adult required" }),
+  children: z.number().min(0),
 });
 
 type ContactFormValues = z.infer<typeof contactFormSchema>;
@@ -51,9 +53,15 @@ export default function BookingModal({
       email: "",
       phone: "",
       message: "",
+      adults: 1,
+      children: 0,
     },
   });
+  const adults = form.watch("adults");
+  const children = form.watch("children");
 
+  const setAdults = (value: number) => form.setValue("adults", value);
+  const setChildren = (value: number) => form.setValue("children", value);
   // Submit handler
   function onSubmit(values: ContactFormValues) {
     setIsSubmitting(true);
@@ -86,108 +94,154 @@ export default function BookingModal({
 
   return (
     <div>
-      <h2 className="font-semibold text-xl text-center pb-4">Confirm Your Booking</h2>
+      <h2 className="font-semibold text-xl text-center pb-4">
+        Confirm Your Booking
+      </h2>
       <div className="flex gap-4 p-3 border bg-green-50 rounded-xl">
-        <Image className="w-[100px] rounded-xl" src={RoomImg1} alt=""/>
+        <Image className="w-[100px] rounded-xl" src={RoomImg1} alt="" />
         <div>
-        <h3 className="font-semibold pb-2 text-xl">Super Deluxe Room</h3>
-        <h4 className="text-sm font-semibold inter-font">
-        $100-240$ <span className="text-xs text-gray-500">/night</span>
-      </h4>
-
+          <h3 className="font-semibold pb-2 text-xl">Super Deluxe Room</h3>
+          <h4 className="text-sm font-semibold inter-font">
+            $100-240$ <span className="text-xs text-gray-500">/night</span>
+          </h4>
         </div>
       </div>
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="space-y-4 py-4"
-          >
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Name</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Your name" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input type="email" placeholder="Your email" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={form.control}
-              name="phone"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Phone</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="tel"
-                      placeholder="Your phone number"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={form.control}
-              name="message"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Message</FormLabel>
-                  <FormControl>
-                    <Textarea
-                      placeholder="Tell us about your desired stay dates and any special requests"
-                      className="min-h-[100px]"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            {formStatus.message && (
-              <div
-                className={`p-3 rounded-md ${
-                  formStatus.success
-                    ? "bg-green-50 text-green-700"
-                    : "bg-red-50 text-red-700"
-                }`}
-              >
-                {formStatus.message}
-              </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 py-4">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Your name" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
             )}
+          />
 
-              <Button
-                type="submit"
-                className="w-full py-6! text-lg"
-                variant={'primary'}
-                disabled={isSubmitting}
-              >
-                {isSubmitting ? "Sending..." : "Send Inquiry"}
-              </Button>
-          </form>
-        </Form>
-        </div>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input type="email" placeholder="Your email" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="phone"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Phone</FormLabel>
+                <FormControl>
+                  <Input
+                    type="tel"
+                    placeholder="Your phone number"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className="grid grid-cols-2 md:grid-cols-2 gap-4">
+            <GuestSelector
+              title="Adults"
+              guests={adults}
+              setGuests={(val) => setAdults(val)}
+            />
+            <GuestSelector
+              title="Children"
+              guests={children}
+              setGuests={(val) => setChildren(val)}
+            />
+          </div>
+
+          <FormField
+            control={form.control}
+            name="message"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Message</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="Tell us about your desired stay dates and any special requests"
+                    className="min-h-[100px]"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {formStatus.message && (
+            <div
+              className={`p-3 rounded-md ${
+                formStatus.success
+                  ? "bg-green-50 text-green-700"
+                  : "bg-red-50 text-red-700"
+              }`}
+            >
+              {formStatus.message}
+            </div>
+          )}
+
+          <Button
+            type="submit"
+            className="w-full py-6! text-lg"
+            variant={"primary"}
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "Sending..." : "Send Inquiry"}
+          </Button>
+        </form>
+      </Form>
+    </div>
   );
 }
+const GuestSelector = ({
+  setGuests,
+  guests,
+  title,
+}: {
+  guests: number;
+  title: string;
+  setGuests: (value: number) => void;
+}) => {
+  return (
+    <div className=" w-fit">
+      <p className="text-xs mb-2 md:text-sm font-medium text-primary">{title}</p>
+      <div className="flex  border px-4 rounded-full py-1 w-fit justify-center items-center gap-x-4">
+        <button
+          type="button"
+          aria-label="decrease"
+          className="cursor-pointer"
+          onClick={() => guests > 1 && setGuests(guests - 1)}
+        >
+          <MinusIcon />
+        </button>
+        <p className="text-sm md:text-xl font-medium">{guests}</p>
+
+        <button
+          type="button"
+          aria-label="increase"
+          className="cursor-pointer"
+          onClick={() => setGuests(guests + 1)}
+        >
+          <Plus />
+        </button>
+      </div>
+    </div>
+  );
+};
